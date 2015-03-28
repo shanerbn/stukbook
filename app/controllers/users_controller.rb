2b93129acf273f01f3260796dd0e103a83ce12dd
shanerbn <shane@converted.co.uk>
2015-03-28 00:03:12 +0000

    fumbling with add posts issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only:[:index]
   before_action :set_user, only:[:show]
-  # before_action :get_counts, only: [:index]
+  before_action :get_counts, only: [:index]
 
   def index
     case params[:people] when "friends"
-        @users = current_user.active_friends
+                           @users = current_user.active_friends
       when "requests"
         @users = current_user.pending_friend_requests_from.map(&:user)
       when "pending"
@@ -17,12 +17,49 @@ class UsersController < ApplicationController
 
   def show
     @post = Post.new
-    @activities = PublicActivity::Activity.where(owner_id: @user.id) + PublicActivity::Activity.where(recipient_id: @user.id)
+    @posts = @user.posts.order('created_at DESC')
+    @activities = PublicActivity::Activity.where(owner_id: @user.id).order('created_at DESC')
   end
 
   private
+
+  def get_counts
+    @friend_count = current_user.active_friends.size
+    @pending_count = current_user.pending_friend_requests_to.map(&:friend).size
+  end
 
   def set_user
     @user = User.find_by(username: params[:id])
   end
 end
+
+
+# class UsersController < ApplicationController
+#   before_action :authenticate_user!, only:[:index]
+#   before_action :set_user, only:[:show]
+#   # before_action :get_counts, only: [:index]
+#
+#   def index
+#     case params[:people] when "friends"
+#         @users = current_user.active_friends
+#       when "requests"
+#         @users = current_user.pending_friend_requests_from.map(&:user)
+#       when "pending"
+#         @users = current_user.pending_friend_requests_to.map(&:friend)
+#       else
+#         @users = User.where.not(id: current_user.id)
+#     end
+#   end
+#
+#   def show
+#     @post = Post.new
+#     @posts = @user.posts
+#     @activities = PublicActivity::Activity.where(owner_id: @user.id) + PublicActivity::Activity.where(recipient_id: @user.id)
+#   end
+#
+#   private
+#
+#   def set_user
+#     @user = User.find_by(username: params[:id])
+#   end
+# end
